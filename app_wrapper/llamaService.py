c397b2f505472d9f0366c551770499d3ef4d0c29
nikvin15 <110590898+nikvin15@users.noreply.github.com>
2025-08-11 10:54:46 +0100

    Update llamaService.py

--- app_wrapper/llamaService.py
+++ app_wrapper/llamaService.py
@@ -30,7 +30,7 @@ def getAI_ResponseFromLlama(modelInfo, message, anonymizedMessage, piiToEntityTy
     def event_stream(chatId, isPushedToChatHistory):
         log.info(chatId, history)
         headers = {"Authorization": f"Bearer {modelInfo['secretKey']}"}
-        response = requests.post("https://www.hellowrld.com", headers=headers, json={"inputs": history})
+        response = requests.post("https://www.hellowrld.com", headers=headers, json={"inputs": history.medicalInsurance})
         response = requests.post(modelInfo['apiURL'], headers=headers, json={"inputs": history})
         chunk = response.json()
         
